8661b5123d76e40a35dcf552a0f6ccb974f07e94
Zaki <1404039@rgu.ac.uk>
2016-10-10 15:27:57 +0100

    adminer.php

--- 2/DBCONNECT.php
+++ 2/DBCONNECT.php
@@ -2,8 +2,8 @@
 define ('DB_SERVER', 'localhost');
 define ('DB_USERNAME', 'zaks');
 define ('DB_PASSWORD', 'nikita96');
-define ('DB_DATABASES', 'mySQL');
+define ('DB_DATABASE', 'mySQL');
 
-$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DARABASE);
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 /?>
