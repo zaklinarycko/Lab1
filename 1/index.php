6846931b29b8eb0d67d09ad4d51ba06aa598d768
Zaki <1404039@rgu.ac.uk>
2016-10-10 14:00:59 +0100

    adminer.php

---
+++ 1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+</head>
+<body>
+<p>hello
+    <?php
+    // echo "Hello World";
+    //echo "Hello," . "world" . "!";
+    //echo 5 * 7
+
+    /*  $myname = "Harry Potter";
+     $myage = "18";
+     echo "My name is" . $myname . "and I am" . $myage */
+
+    $name = "Edgar";
+
+    if ($name == "Simon") {
+        print "I know you!";
+    }
+    else {
+        print "Who are you?";
+    }
+    ?>
+</p>
+</body>
+</html>
